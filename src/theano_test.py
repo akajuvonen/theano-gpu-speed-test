10c2d21208e8d73e956a9273d66c1171e234c137
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-04-12 08:27:33 +0300

    Added theano function for adding matrices to main

--- src/theano_test.py
+++ src/theano_test.py
@@ -5,7 +5,11 @@ import theano.tensor as T
 from theano import function
 
 def main():
-  print('test')
+  # A simple theano function to add to matrices
+  x = T.dmatrix('x')
+  y = T.dmatrix('y')
+  z = x + y
+  f = function([x, y], z)
 
 if __name__=="__main__":
   main()
