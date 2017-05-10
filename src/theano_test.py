47f67286ace60f549673a450ece92edbef11eed2
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-10 08:44:11 +0300

    Changed the python file indentation from 2 to 4

--- src/theano_test.py
+++ src/theano_test.py
@@ -5,17 +5,17 @@ import theano.tensor as T
 from theano import function
 
 def main():
-  # A simple theano function to add to matrices
-  x = T.dmatrix('x')
-  y = T.dmatrix('y')
-  z = x + y
-  f = function([x, y], z)
+    # A simple theano function to add to matrices
+    x = T.dmatrix('x')
+    y = T.dmatrix('y')
+    z = x + y
+    f = function([x, y], z)
 
-  # Add two random matrices together
-  m1 = np.random.rand(10, 10)
-  m2 = np.random.rand(10, 10)
-  result = f(m1, m2)
-  print(result)
+    # Add two random matrices together
+    m1 = np.random.rand(10, 10)
+    m2 = np.random.rand(10, 10)
+    result = f(m1, m2)
+    print(result)
 
 if __name__=="__main__":
   main()
