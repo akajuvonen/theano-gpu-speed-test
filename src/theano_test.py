0c57da9e82e93a99cd6c130aeb46a5c9618a5321
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-11 08:20:19 +0300

    Increased the matrix size

--- src/theano_test.py
+++ src/theano_test.py
@@ -12,8 +12,8 @@ def main():
     f = function([x, y], z)
 
     # Multiply two random matrices
-    m1 = np.random.rand(10, 10)
-    m2 = np.random.rand(10, 10)
+    m1 = np.random.rand(1000, 1000)
+    m2 = np.random.rand(1000, 1000)
     result = f(m1, m2)
 
 if __name__=="__main__":
