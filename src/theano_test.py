19ca06cb0cb4fa047d0de9bb8761584a5dd70d0a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-16 10:07:30 +0300

    Added time calculation in python script

--- src/theano_test.py
+++ src/theano_test.py
@@ -3,6 +3,7 @@
 import numpy as np
 import theano.tensor as T
 from theano import function
+import time
 
 
 def main():
@@ -12,10 +13,19 @@ def main():
     z = x * y
     f = function([x, y], z)
 
+    # Start time
+    time0 = time.time()
+
     # Multiply two random matrices
     m1 = np.random.rand(10000, 10000)
     m2 = np.random.rand(10000, 10000)
     result = f(m1, m2)
+
+    # End time
+    time1 = time.time()
+
+    # Elapsed time
+    print('Elapsed time: %f' % (time1 - time0))
 
 if __name__ == "__main__":
     main()
