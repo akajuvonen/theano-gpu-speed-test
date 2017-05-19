281ab443ad7825ccc1ccbc5ee375eca2554dc3ea
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-19 10:10:56 +0300

    Theano main now changes environment variables

--- src/theano_main.py
+++ src/theano_main.py
@@ -8,6 +8,7 @@ from multiprocessing import Process, Queue
 def main():
     # Run theano processing and print elapsed time
     # CPU
+    print('--- USING CPU ---')
     os.environ['THEANO_FLAGS'] = 'device=cpu'
     q = Queue()
     p = Process(target=process, args=(q,))
@@ -15,13 +16,16 @@ def main():
     p.join()
     timecpu = q.get()
     # GPU
+    print('--- USING GPU ---')
     os.environ['THEANO_FLAGS'] = 'device=cuda'
-    #time2 = process()
+    q = Queue()
+    p = Process(target=process, args=(q,))
+    p.start()
+    p.join()
+    timegpu = q.get()
     # Print times
-    # NOTE: This will not work yet, need to spawn separate Processes
-    # that init theano again
     print('Time used with CPU: %f' % timecpu)
-    #print('Time used with GPU: %f' % time2)
+    print('Time used with GPU: %f' % timegpu)
 
 
 if __name__ == "__main__":
