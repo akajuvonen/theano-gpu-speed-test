7adc2e5b82419ec836a3a03f60cccd8ddeb7c368
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-19 09:52:47 +0300

    Changed theano CPU and GPU time printing

--- src/theano_main.py
+++ src/theano_main.py
@@ -8,12 +8,15 @@ def main():
     # Run theano processing and print elapsed time
     # CPU
     os.environ['THEANO_FLAGS'] = 'device=cpu'
-    time = process()
-    print(time)
+    time1 = process()
     # GPU
     os.environ['THEANO_FLAGS'] = 'device=cuda'
-    time = process()
-    print(time)
+    time2 = process()
+    # Print times
+    # NOTE: This will not work yet, need to spawn separate Processes
+    # that init theano again
+    print('Time used with CPU: %f' % time1)
+    print('Time used with GPU: %f' % time2)
 
 
 if __name__ == "__main__":
