3d869095f285b01d1d06118edc14c6f813afd9a1
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-05-19 09:47:48 +0300

    Theano main script now tries to change theano device and run with cpu and gpu

--- src/theano_main.py
+++ src/theano_main.py
@@ -1,12 +1,20 @@
 #!/urs/bin/env python
 
 from theano_process import process
+import os
 
 
 def main():
     # Run theano processing and print elapsed time
+    # CPU
+    os.environ['THEANO_FLAGS'] = 'device=cpu'
     time = process()
     print(time)
+    # GPU
+    os.environ['THEANO_FLAGS'] = 'device=cuda'
+    time = process()
+    print(time)
+
 
 if __name__ == "__main__":
     main()
